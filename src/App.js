1c54fd1c8b483ccea95cf3342807fd374e100947
Avi-J0506 <92037313+Avi-J0506@users.noreply.github.com>
2022-11-04 14:12:27 +0530

    basic changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import {Navbar} from './Components/Navbar';
+// import {Navbar} from './Components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
